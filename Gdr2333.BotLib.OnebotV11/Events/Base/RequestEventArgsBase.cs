eba9171a5022986f15332cde06a22ed3e9c1809f
df1050 <df1050@126.com>
2025-07-24 01:09:45 +0800

    请求事件：完成

---
+++ Gdr2333.BotLib.OnebotV11/Events/Base/RequestEventArgsBase.cs
@@ -0,0 +1,83 @@
+/*
+   Copyright 2025 All contributors of Gdr2333.BotLib
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Gdr2333.BotLib.OnebotV11.Events.Interfaces;
+using Gdr2333.BotLib.OnebotV11.Utils;
+
+namespace Gdr2333.BotLib.OnebotV11.Events.Base;
+
+/// <summary>
+/// 请求事件基类
+/// </summary>
+public abstract class RequestEventArgsBase : OnebotV11EventArgsBase, IUserEventArgs
+{
+    /// <summary>
+    /// 请求类型
+    /// </summary>
+    [JsonInclude, JsonRequired, JsonPropertyName("request_type")]
+    public RequestType Type { get; internal set; }
+
+    /// <summary>
+    /// 发起请求的用户Id
+    /// </summary>
+    [JsonInclude, JsonRequired, JsonPropertyName("user_id"), JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public long UserId { get; internal set; }
+
+    /// <summary>
+    /// 请求Flag
+    /// </summary>
+    [JsonInclude, JsonRequired, JsonPropertyName("flag")]
+    public string Flag { get; internal set; } = string.Empty;
+}
+
+/// <summary>
+/// 请求类型
+/// </summary>
+[JsonConverter(typeof(RequestTypeConverter))]
+public enum RequestType
+{
+    /// <summary>
+    /// 好友请求
+    /// </summary>
+    Friend,
+    /// <summary>
+    /// 请求加群 或 邀请进群
+    /// </summary>
+    Group
+}
+
+internal class RequestTypeConverter : JsonConverter<RequestType>
+{
+    public override RequestType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.GetString()?.ToLower() switch
+        {
+            "friend" => RequestType.Friend,
+            "group" => RequestType.Group,
+            _ => throw new InvalidDataException(StaticData.BadEnumValueMessage)
+        };
+
+    public override void Write(Utf8JsonWriter writer, RequestType value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value switch
+        {
+            RequestType.Friend => "friend",
+            RequestType.Group => "group",
+            _ => throw new InvalidDataException(StaticData.BadEnumValueMessage)
+        });
+    }
+}
---
+++ Gdr2333.BotLib.OnebotV11/Events/GroupAddRequestEventArgs.cs
@@ -0,0 +1,84 @@
+/*
+   Copyright 2025 All contributors of Gdr2333.BotLib
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Gdr2333.BotLib.OnebotV11.Events.Base;
+using Gdr2333.BotLib.OnebotV11.Events.Interfaces;
+using Gdr2333.BotLib.OnebotV11.Utils;
+
+namespace Gdr2333.BotLib.OnebotV11.Events;
+
+/// <summary>
+/// （加群请求 或 邀请加群）事件参数
+/// </summary>
+public class GroupAddRequestEventArgs : RequestEventArgsBase, IGroupEventArgs
+{
+    /// <summary>
+    /// 子类型（加群请求 或 邀请加群）
+    /// </summary>
+    [JsonInclude, JsonRequired, JsonPropertyName("sub_type")]
+    public GroupAddRequestType Subtype { get; internal set; }
+
+    /// <summary>
+    /// 群聊Id
+    /// </summary>
+    [JsonInclude, JsonRequired, JsonPropertyName("group_id"), JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public long GroupId { get; internal set; }
+
+    /// <summary>
+    /// 验证信息
+    /// </summary>
+    [JsonInclude, JsonPropertyName("comment")]
+    public string? Comment { get; internal set; }
+}
+
+/// <summary>
+/// 加群相关请求子类型（加群请求 或 邀请加群）
+/// </summary>
+[JsonConverter(typeof(GroupAddRequestTypeConverter))]
+public enum GroupAddRequestType
+{
+    /// <summary>
+    /// 加群请求
+    /// </summary>
+    Request,
+    /// <summary>
+    /// 邀请加群
+    /// </summary>
+    Invite
+}
+
+internal class GroupAddRequestTypeConverter : JsonConverter<GroupAddRequestType>
+{
+    public override GroupAddRequestType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.GetString()?.ToLower() switch
+        {
+            "add" => GroupAddRequestType.Request,
+            "invite" => GroupAddRequestType.Invite,
+            _ => throw new InvalidDataException(StaticData.BadEnumValueMessage)
+        };
+
+    public override void Write(Utf8JsonWriter writer, GroupAddRequestType value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value switch
+        {
+            GroupAddRequestType.Request => "add",
+            GroupAddRequestType.Invite => "invite",
+            _ => throw new InvalidDataException(StaticData.BadEnumValueMessage)
+        });
+    }
+}
